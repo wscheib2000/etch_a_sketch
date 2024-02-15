092bdb79665a494a51ae41e12ce4c0b6c55d1131
wscheib2000 <william.scheib@gatech.edu>
2024-02-15 15:40:06 -0500

    Reorganizing

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_clockwise():
+    tim.right(10)
+
+def turn_counter_clockwise():
+    tim.left(10)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_counter_clockwise)
+screen.onkey(key="d", fun=turn_clockwise)
+screen.onkey(key="c", fun=clear)
+screen.exitonclick()
